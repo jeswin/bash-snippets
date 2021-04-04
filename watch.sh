5948045b85e91ee86daaf2fe008a0ab42e59a00a
Jeswin <jeswinpk@agilehead.com>
2021-04-04 08:56:55 +0530

    Watch script

--- watch.sh
+++ watch.sh
@@ -4,6 +4,7 @@
 # ./watch.sh $src_code_dir $build_command $kill_command $restart_command
 # ./watch.sh 'src/' 'npm run build' 'killall http-server' 'http-server dist/ &'
 
+eval $4
 while inotifywait -r -e modify -e create $1; do
   eval $2
   eval $3
